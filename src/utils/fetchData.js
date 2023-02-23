1d847600412a5307767797535dda91416bba0d9a
Abhishek Chatterjee <chatterjee.85@gmail.com>
2023-02-23 17:11:53 +0530

    First commit for github

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key': 'a633d1e8bamsh1b6df539bd94586p1f1e53jsnef910eade953',
+    },
+  };
+  
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': 'a633d1e8bamsh1b6df539bd94586p1f1e53jsnef910eade953',
+    },
+  };
+  
+  export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
+    return data;
+  };
